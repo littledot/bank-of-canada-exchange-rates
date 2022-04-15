168fc77e4969c9c9c2f7b621d8ad6d1ba88b3c46
littledot <littledot5566@gmail.com>
2022-04-15 16:38:41 -0400

    refmt output

--- dl_forex.py
+++ dl_forex.py
@@ -1,29 +1,71 @@
 import requests
 import shutil
 import functools
+import json
 
 # https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates-lookup/
 
-# api: https://www.bankofcanada.ca/valet/observations/FXAUDCAD,FXBRLCAD,FXCNYCAD,FXEURCAD,FXHKDCAD,FXINRCAD,FXIDRCAD,FXJPYCAD,FXMYRCAD,FXMXNCAD,FXNZDCAD,FXNOKCAD,FXPENCAD,FXRUBCAD,FXSARCAD,FXSGDCAD,FXZARCAD,FXKRWCAD,FXSEKCAD,FXCHFCAD,FXTWDCAD,FXTHBCAD,FXTRYCAD,FXGBPCAD,FXUSDCAD,FXVNDCAD/json?start_date=2022-03-30&end_date=2022-04-07
+dailyUrlFmt = "https://www.bankofcanada.ca/valet/observations/FXAUDCAD,FXBRLCAD,FXCNYCAD,FXEURCAD,FXHKDCAD,FXINRCAD,FXIDRCAD,FXJPYCAD,FXMYRCAD,FXMXNCAD,FXNZDCAD,FXNOKCAD,FXPENCAD,FXRUBCAD,FXSARCAD,FXSGDCAD,FXZARCAD,FXKRWCAD,FXSEKCAD,FXCHFCAD,FXTWDCAD,FXTHBCAD,FXTRYCAD,FXGBPCAD,FXUSDCAD,FXVNDCAD/json?start_date={start}&end_date={end}"
 
-urlFmt = "https://www.bankofcanada.ca/valet/observations/FXAUDCAD,FXBRLCAD,FXCNYCAD,FXEURCAD,FXHKDCAD,FXINRCAD,FXIDRCAD,FXJPYCAD,FXMYRCAD,FXMXNCAD,FXNZDCAD,FXNOKCAD,FXPENCAD,FXRUBCAD,FXSARCAD,FXSGDCAD,FXZARCAD,FXKRWCAD,FXSEKCAD,FXCHFCAD,FXTWDCAD,FXTHBCAD,FXTRYCAD,FXGBPCAD,FXUSDCAD,FXVNDCAD/json?start_date={start}&end_date={end}"
+yearUrl = "https://www.bankofcanada.ca/valet/observations/group/FX_RATES_ANNUAL/json?start_date=2017-01-01"
 
 
-def download_file(url, dest):
+def downloadFile(url, path):
     with requests.get(url, stream=True) as r:
-        with open(dest, "wb") as f:
-            r.raw.read = functools.partial(r.raw.read, decode_content=True)
+        with open(path, "wb") as f:
+            r.raw.read = functools.partial(r.raw.read, decode_content=True)  # decodes gzip
             shutil.copyfileobj(r.raw, f)
 
 
-def download_forex(year):
-    url = urlFmt.format(start="{}-12-25".format(year - 1), end="{}-01-01".format(year + 1))
-    download_file(url, "cad_{}.json".format(year))
+def downloadForex(year, path):
+    url = dailyUrlFmt.format(start="{}-12-25".format(year - 1), end="{}-01-01".format(year + 1))
+    downloadFile(url, path)
+    return path
+
+
+def cleanData(names):
+    with open(names["raw"], "r") as inFile:
+        inJson = json.load(inFile)
+
+        allObs = {}
+        outJson = {
+            "terms": inJson["terms"],
+            "seriesDetail": inJson["seriesDetail"],
+            "observations": allObs,
+        }
+
+        for inObs in inJson["observations"]:
+            date = inObs.pop("d")
+            outObs = {}
+            allObs[date] = outObs
+
+            for currency, rate in inObs.items():
+                outObs[currency] = rate["v"]
+
+        with open(names["clean"], "w") as outFile:
+            json.dump(outJson, outFile)
+
+        with open(names["pretty"], "w") as outFile:
+            json.dump(outJson, outFile, indent=4)
+
+
+def getFileNames(name):
+    return {
+        "raw": name + ".raw.json",
+        "clean": name + ".json",
+        "pretty": name + ".pretty.json",
+    }
 
 
 def main():
-    for i in range(2020, 2022):
-        download_forex(i)
+    for i in range(2017, 2022):
+        names = getFileNames("data/boc_" + str(i))
+        downloadForex(i, names["raw"])
+        cleanData(names)
+
+    names = getFileNames("data/boc_annual")
+    downloadFile(yearUrl, names["raw"])
+    cleanData(names)
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,9 +1,15 @@
 # Bank of Canada Foreign Exchange Rates
 
-Data source: https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates-lookup/
+Data source: 
+- Daily: https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates-lookup/
+- Yearly: https://www.bankofcanada.ca/rates/exchange/annual-average-exchange-rates/
 
-Base url: https://littledot.github.io/bank-of-canada-exchange-rates/
+Daily exchange rates: https://littledot.github.io/bank-of-canada-exchange-rates/data/boc_2017.json
 
-Daily exchange rates: https://littledot.github.io/bank-of-canada-exchange-rates/data/daily/boc_2017.json
+Yearly average rates: https://littledot.github.io/bank-of-canada-exchange-rates/data/boc_annual.json
 
-Yearly average rates: https://littledot.github.io/bank-of-canada-exchange-rates/data/boc_annual_rate.json
+boc_{year}.raw.json: Original file from the data source
+
+boc_{year}.pretty.json: Reformatted file
+
+boc_{year}.json: Reformatted with no extra whitespace
